769318b57afd814226350fe5bd576f5c21ee83a3
Tomasz Gil <tomaszgil.dev@gmail.com>
2021-01-10 14:30:54 +0100

    Create homepage

---
+++ src/screens/NotFound.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const NotFound: React.FC = () => {
+  return <div>Not found</div>
+}
+
+export default NotFound
---
+++ src/styles/heading.js
@@ -0,0 +1,7 @@
+const Heading = {
+  baseStyle: {
+    color: 'brand.900',
+  },
+}
+
+export default Heading
